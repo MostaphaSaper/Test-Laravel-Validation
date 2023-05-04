c374685790e5f5abcae1d978840d90c1fde7781f
MostafaSaber17 <mostaphasaper17@gmail.com>
2023-05-04 13:11:08 +0300

    aa

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use App\Rules\Uppercase;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => ['required', new Uppercase],
+        ]);
+        Article::create(['title' => $request->title]);
+    }
+}
